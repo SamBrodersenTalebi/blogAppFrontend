7263fddfcc93a373f88e64322819fc63b57d9987
samtalebi <samtalebi84@gmail.com>
2020-04-25 17:10:17 +0200

    add proptype to loginform

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const LoginForm = ({ onSubmit, handleChange, password, username }) => {
   return (
@@ -28,6 +29,14 @@ const LoginForm = ({ onSubmit, handleChange, password, username }) => {
       </form>
     </div>
   );
+};
+
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
 };
 
 export default LoginForm;
