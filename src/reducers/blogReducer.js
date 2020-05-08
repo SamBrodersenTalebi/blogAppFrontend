a4457c75e6036a9747cb0e4a711165fff5effdd7
samtalebi <samtalebi84@gmail.com>
2020-05-08 12:23:38 +0200

    createBlog action

--- src/reducers/blogReducer.js
+++ src/reducers/blogReducer.js
@@ -4,18 +4,36 @@ const reducer = (state = [], action) => {
   switch (action.type) {
     case 'INIT':
       return action.data;
+    case 'CREATE':
+      //could use concat return new array!
+      [...state, action.data];
     default:
       return state;
   }
 };
 
-//actions
+/*
+ACTIONS
+*/
+
+//Get inital blogs
 export const initialBlogs = (blogs) => {
   return async (dispatch) => {
     const anecdotes = await blogService.getAll();
     dispatch({
       type: 'INIT',
       data: anecdotes,
+    });
+  };
+};
+
+//create a new blog
+export const createBlog = (blog) => {
+  return async (dispatch) => {
+    const newBlog = await blogService.create(blog);
+    dispatch({
+      type: 'CREATE',
+      data: newBlog,
     });
   };
 };
