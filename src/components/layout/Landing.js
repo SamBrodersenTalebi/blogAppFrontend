8283e38d659b4a2aa4b8b17c8570ed9e46b1ec61
samtalebi <samtalebi84@gmail.com>
2020-05-18 20:36:58 +0200

    landing header

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -4,29 +4,35 @@ import { Button } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 import BlogList from '../blogs/BlogList';
 import BlogFrom from '../blogs/BlogForm';
+import './layout.css';
 
 const Landing = () => {
   const user = useSelector((state) => state.auth.user);
 
   return (
     <div>
-      <h2>
-        Welcome to Sams Blog application where you can customize and create your{' '}
-        <i>own blogs</i> from scratch
-      </h2>
-      {user ? (
-        <p>Welcome {user.name}</p>
-      ) : (
-        <div>
-          <p>Create your own user or login if you already have one</p>
-          <Button variant='contained' color='primary'>
-            <Link to='register'>Sign Up</Link>
-          </Button>
-          <Button variant='contained'>
-            <Link to='login'>Log in</Link>
-          </Button>
-        </div>
-      )}
+      <div style={{ margin: 88 }}>
+        <h2 className='center roboto' id='heading'>
+          Welcome to Sams Blog application where you can customize and create
+          your <i style={{ fontFamily: 'monospace' }}>own blogs</i> from scratch
+        </h2>
+        {user ? (
+          <p className='center' style={{ fontSize: 20 }}>
+            Welcome <i id='name-ani'>{user.name}</i>
+          </p>
+        ) : (
+          <div>
+            <p>Create your own user or login if you already have one</p>
+            <Button variant='contained' color='primary'>
+              <Link to='register'>Sign Up</Link>
+            </Button>
+            <Button variant='contained'>
+              <Link to='login'>Log in</Link>
+            </Button>
+          </div>
+        )}
+      </div>
+
       <BlogList />
       <BlogFrom />
     </div>
